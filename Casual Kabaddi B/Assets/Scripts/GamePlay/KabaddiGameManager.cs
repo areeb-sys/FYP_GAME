1430a54b9145f0c2adf1a76fb06993d5b4a0476d
Syed Omer <omer.s@itcomp.org>
2023-04-08 13:56:48 +0500

    XP Manager
    Maintain XP of Player and decide easy, medium. hard mode for the game

---
+++ Casual Kabaddi B/Assets/Scripts/GamePlay/KabaddiGameManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KabaddiGameManager : MonoBehaviour
+{
+    public GameObject playerTeam;
+    public GameObject opponentTeam;
+
+    public GameObject HeadTailPanel;
+    public TMP_Text tossResultText;
+
+    public GameObject headsButton, tailsButton;
+
+    private bool playerTeamGetsFirstRaid;
+
+    public XPManager xPManager;
+
+    public GameObject MenuPanel;
+
+    public void StartGame()
+    {
+        // Ask the player to choose heads or tails
+        tossResultText.text = "Choose heads or tails.";
+
+        AskPlayerForHeadTail();
+    }
+
+
+    private void AskPlayerForHeadTail()
+    {
+        HeadTailPanel.SetActive(true);
+        MenuPanel.SetActive(false);
+    }
+
+    public void  ChooseHead(bool playerChoseHeads)
+    {
+        headsButton.SetActive(false);
+        tailsButton.SetActive(false);
+        // Randomly determine the result of the toss
+        bool tossResultIsHeads = Random.value > 0.5f;
+
+        // Determine if the player won the toss
+        if (playerChoseHeads == tossResultIsHeads)
+        {
+            playerTeamGetsFirstRaid = true;
+            tossResultText.text = "You won the toss! Choose to raid first or defend.";
+        }
+        else
+        {
+            playerTeamGetsFirstRaid = false;
+            tossResultText.text = "Opponent won the toss and chose to raid first. Defend!";
+        }
+
+        // Assign player and opponent team members
+        AssignTeamMembers(playerTeam, 7);
+        AssignTeamMembers(opponentTeam, 7);
+    }
+
+    void AssignTeamMembers(GameObject team, int numMembers)
+    {
+        // Iterate through the team's child objects and activate the first 'numMembers' members
+        for (int i = 0; i < team.transform.childCount; i++)
+        {
+            if (i < numMembers)
+            {
+                team.transform.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                team.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public bool DoesPlayerTeamGetFirstRaid()
+    {
+        return playerTeamGetsFirstRaid;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Casual Kabaddi B/Assets/Scripts/XPManager.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public enum GameMode
+{
+    Easy,
+    Medium,
+    Hard
+}
+
+public class XPManager : MonoBehaviour
+{
+    public int xp = 0;
+
+    public GameMode gameMode = GameMode.Easy;
+
+    public Image xpBarImage;
+    public TMP_Text xpText;
+    public TMP_Text modeText;
+
+    private int maxXp;
+
+    private void Start()
+    {
+        // Load XP from player preferences or other storage mechanism
+        xp = PlayerPrefs.GetInt("XP", 0);
+
+        // Set game mode based on XP
+        if (xp < 500)
+        {
+            gameMode = GameMode.Easy;
+        }
+        else if (xp < 2000)
+        {
+            gameMode = GameMode.Medium;
+        }
+        else
+        {
+            gameMode = GameMode.Hard;
+        }
+        UpdateXPBar();
+    }
+
+    public void AddXP(int amount)
+    {
+        xp += amount;
+        if (xp < 0) return;
+
+        // Update game mode based on new XP
+        if (xp < 500)
+        {
+            gameMode = GameMode.Easy;
+        }
+        else if (xp < 2000)
+        {
+            gameMode = GameMode.Medium;
+        }
+        else if(xp < 3000)
+        {
+            gameMode = GameMode.Hard;
+        }
+        if (xp > 3000) xp = 3000;
+        // Save XP to player preferences or other storage mechanism
+        PlayerPrefs.SetInt("XP", xp);
+        UpdateXPBar();
+    }
+
+    private void UpdateXPBar()
+    {
+        maxXp = GetMaxXP(gameMode);
+        // Update XP bar fill based on current XP and max XP for the current game mode
+        float fillAmount = (float)xp / maxXp;
+        xpBarImage.fillAmount = fillAmount;
+        xpText.text = string.Format("{0}/{1}", xp, (int)maxXp);
+        modeText.text = gameMode.ToString();
+    }
+
+    public int GetMaxXP(GameMode gameMode)
+    {
+        switch (gameMode)
+        {
+            case GameMode.Easy:
+                return 500;
+            case GameMode.Medium:
+                return 1500;
+            case GameMode.Hard:
+                return 3000;
+            default:
+                return 0;
+        }
+    }
+}
